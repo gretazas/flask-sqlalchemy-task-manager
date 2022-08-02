b5ab298a9d8ed30067dd16abdd4deb98d87f61a2
gretazas <esminis@yahoo.com>
2022-08-02 16:20:07 +0000

    Add changes

---
+++ taskmanager/__init__.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+if os.path.exists("env.py"):
+    import env  # noqa
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from taskmanager import routes  # noqa
