6958a196bf89fd4117cea98a41d3e48670e9c4ed
gretazas <esminis@yahoo.com>
2022-08-02 16:46:57 +0000

    Changes in __init__.py

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -2,7 +2,6 @@ import os
 import re
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 if os.path.exists("env.py"):
     import env  # noqa
 
@@ -11,13 +10,12 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 if os.environ.get("DEVELOPMENT") == "True":
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")  # local
 else:
     uri = os.environ.get("DATABASE_URL")
-    if uri.startswith("postgress://"):
+    if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
-    app.config["SQLALCHEMY_DATABASE_URI"] = uri
-
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
